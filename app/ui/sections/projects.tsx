55339bd05b7cb18eebe2afde445b0a4cdf39f996
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2026-07-04 00:48:45 +0100

    separate web portfolio from mobile portfolio

--- app/ui/sections/projects.tsx
+++ app/ui/sections/projects.tsx
@@ -13,7 +13,7 @@ export const Projects = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const platformFilter =
-    (searchParams.get("platform") as "web" | "mobile") || "mobile";
+    (searchParams.get("platform") as "web" | "mobile") || "web";
 
   const handleFilterChange = (newPlatform: string) => {
     const params = new URLSearchParams(searchParams);
@@ -24,25 +24,6 @@ export const Projects = () => {
   return (
     <Suspense fallback={<Loading />}>
       <section id="projects" className="pattern-before py-8">
-        <div className="flex gap-4 mb-6 md:mb-10 mx-auto w-fit relative">
-          <button
-            onClick={() => handleFilterChange("web")}
-            className={`${stalinistOne.className} text-sm sm:text-lg md:text-2xl ${
-              platformFilter === "web" ? "text-white" : "text-[#FFFFFF80]"
-            } transition duration-200`}
-          >
-            Web Apps
-          </button>
-          <div className="w-0.5 h-4 sm:h-6 md:h-8 bg-[#C4C4C4]"> </div>
-          <button
-            onClick={() => handleFilterChange("mobile")}
-            className={`${stalinistOne.className} text-sm sm:text-lg md:text-2xl ${
-              platformFilter === "mobile" ? "text-white" : "text-[#FFFFFF80]"
-            } transition duration-200`}
-          >
-            Mobile Apps
-          </button>
-        </div>
         {platformFilter === "mobile" ? (
           <>
             <div className="flex flex-col gap-10 md:gap-20 mb-5 md:mb-20 relative">
